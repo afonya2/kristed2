56385749368fa709702deff44652f690b759316b
afonya2 <adamhir@freemail.hu>
2023-04-24 17:57:05 +0200

    finished backend stuff

--- modules/backend.lua
+++ modules/backend.lua
@@ -32,6 +32,25 @@ function returnKrist(trans,amount,message)
     end
 end
 
+function dropItems(id, amount)
+    local remaining = amount
+    local function itemDrop(idd, amountt)
+        for k,v in ipairs(kristed.storages) do
+            for kk,vv in pairs(v.wrap.list()) do
+                if vv.name == id then
+                    local co = v.wrap.pushItems(kristed.config.selfId, kk, amountt, 1)
+                    turtle.drop(amountt)
+                    return co
+                end
+            end
+        end
+    end
+    while remaining > 0 do
+        local ca = itemDrop(id, remaining)
+        remaining = remaining - ca
+    end
+end
+
 function backend()
     sock = initSocket()
 
@@ -41,17 +60,36 @@ function backend()
             trans.meta = kristed.kapi.parseMeta(trans.metadata)
             if mindTrans(trans) then
                 if kristed.checkout.currently then
-                    kristed.checkout.paid = kristed.checkout.paid + trans.value
-                    if kristed.checkout.paid == kristed.checkout.price then
-                        -- Make the item dropping thing
-                    elseif kristed.checkout.paid > kristed.checkout.price then
-                        -- Make the item dropping thing
-                        local dist = math.floor(kristed.checkout.paid - kristed.checkout.price)
-                        if dist >= 1 then
-                            returnKrist(trans, dist, "Thank you for your purchase, here is your change!")
+                    -- Check if there is enough items
+                    local canbe = true
+                    local cbreason = ""
+                    for k,v in ipairs(kristed.checkout.cart) do
+                        if kristed.getItemCount(kristed.config.items[v.item].id) < v.count then
+                            canbe = false
+                            cbreason = kristed.config.items[v.item].name
+                            break
                         end
                     end
-                    os.queueEvent("kristed_rerender")
+
+                    if canbe then
+                        kristed.checkout.paid = kristed.checkout.paid + trans.value
+                        if kristed.checkout.paid == kristed.checkout.price then
+                            for k,v in ipairs(kristed.checkout.cart) do
+                                dropItems(kristed.config.items[v.item].id, v.count)
+                            end
+                        elseif kristed.checkout.paid > kristed.checkout.price then
+                            for k,v in ipairs(kristed.checkout.cart) do
+                                dropItems(kristed.config.items[v.item].id, v.count)
+                            end
+                            local dist = math.floor(kristed.checkout.paid - kristed.checkout.price)
+                            if dist >= 1 then
+                                returnKrist(trans, dist, "Thank you for your purchase, here is your change!")
+                            end
+                        end
+                        os.queueEvent("kristed_rerender")
+                    else
+                        returnKrist(trans, trans.value, "Not enough items: "..cbreason)
+                    end
                 else
                     returnKrist(trans, trans.value, "Currently there is no checkout")
                 end
